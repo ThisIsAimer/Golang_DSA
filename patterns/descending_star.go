f0f67a6c103c58382dd2585d60d564e5c9480e56
raunak <raunakbiswas05@gmail.com>
2025-07-03 12:51:41 +0530

    backward star pattern

---
+++ patterns/descending_star.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+
+    for i := 5; i >= 0; i-- {
+        for j := 0 ; j <= i ; j++ {
+            fmt.Print("*")
+        }
+        fmt.Print("\n")
+    }
+
+}
